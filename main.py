ec56772ca86b422416b3335ab6afb99ad5494cd5
ruyiliang <ruyi.liang@stonybrook.edu>
2023-05-09 18:02:08 +0000

    fixed it

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def preview():
 @app.route('/icd/<value>', methods=['GET'])
 def icdcode(value):
     print('value: ', value)
-    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered = df[df['payer'] == value]
     if len(filtered) <= 0:
         return 'There is nothing here'
     else: 
@@ -26,7 +26,7 @@ def icdcode(value):
 
 @app.route('/icd/<value>/sex/<value2>')
 def icdcode2(value, value2):
-    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered = df[df['payer'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
