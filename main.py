7a7f0d3e96503222db48663881af7025bd24c959
ruyiliang <ruyi.liang@stonybrook.edu>
2023-05-09 21:18:43 +0000

    added again

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ async def preview():
     result = top10rows.to_json(orient="records")
     return {result}
 
-@app.get("/numberscripts/{value}")
-async def numberscriptscode(value):
+@app.get("/rx/{value}")
+async def rxcode(value):
     print('value: ', value)
-    filtered = df[df['NUMBER_SCRIPTS'] == value]
+    filtered = df[df['PAYER'] == value]
     if len(filtered) <= 0:
         return {'There is nothing here.'}
     else: 
         return {filtered.to_json(orient="records")}
 
-@app.get('/numberscripts/{value}/PAYER/{value2}')
-async def numberscriptscode(value, value2):
-    filtered = df[df['NUMBER_SCRIPTS'] == value]
-    filtered2 = filtered[filtered['PAYER'] == value2]
+@app.get('/rx/{value}/TOTAL_SCRIPTS_FILLED_RANK/{value2}')
+async def rxcode2(value, value2):
+    filtered = df[df['PAYER'] == value]
+    filtered2 = filtered[filtered['TOTAL_SCRIPTS_FILLED_RANK'] == value2]
     if len(filtered2) <= 0:
         return {'There is nothing here.'}
     else: 
